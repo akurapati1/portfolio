8dba93a28e9be042cb511fd6b1406e6e793bb0c7
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2024-09-28 20:28:19 -0500

    Adding all the components at once

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import NavBar from "./components/NavBar/navBar";
+import Navbar from "./components/NavBar/navbar";
+import Intro from './components/Intro/intro';
+import Skills from "./components/Skills/skills";
+import Works from "./components/Works/works";
+import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
+
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Intro />
+            <Skills />
+            <Works />
+            <Contact />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
